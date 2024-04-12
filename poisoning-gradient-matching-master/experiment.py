01fdd651ae28e0f895ffd9b26a2d43c7c346cf28
Marco Ruvolo <81305297+mrcruv@users.noreply.github.com>
2024-04-12 12:26:29 +0200

    Update experiment.py
    Fix and some prints added.

--- poisoning-gradient-matching-master/experiment.py
+++ poisoning-gradient-matching-master/experiment.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 
     trainset = CIFAR10(root=args.data_path, train=True, download=True)
     validset = CIFAR10(root=args.data_path, train=False, download=True)
-    poison_ids = list(poison_results["poisons"].keys())
+    poison_ids = [idx.item() for idx in poison_results["poisons"].keys()]
     poison_delta = poison_results["poison_delta"]
     poisonset = Subset(trainset, poison_ids)
 
@@ -33,13 +33,21 @@ if __name__ == "__main__":
     k = 2
 
     # Sample k disjoint subgroups of poison indexes
+    print(f'Sampling {k} poison indexes subgroups')
     poison_ids_subgroups = sample_poisons_ids_subgroups(poison_ids, k)
+    print('Done')
     # Split clean samples indexes by target class
+    print(f'Sampling clean samples indexes by target class')
     clean_ids_by_class = split_clean_ids_by_class(trainset, poison_ids)
+    print('Done')
     # Sample k disjoint subgroups clean samples indexes s.t. the subgroups obtained are (almost) balanced
+    print(f'Sampling {k} clean samples indexes (balanced) subgroups')
     clean_ids_subgroups = sample_clean_ids_subgroups(clean_ids_by_class, n_classes, k)
+    print('Done')
     # Split the poison delta with respect to the poison ids subgroups
+    print(f'Splitting poison delta')
     poison_deltas = split_poison_delta(poisonset, poison_delta, poison_ids_subgroups, k)
+    print('Done')
 
     poisoned_models_stats, clean_models_stats, EPIC_models_stats = [], [], []
     poisoned_models, clean_models, EPIC_models = ([forest.Victim(args, setup=setup) for _ in range(k)],
